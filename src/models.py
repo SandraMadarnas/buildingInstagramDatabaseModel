60e1c867b3698e7775f8471bb08e891323d58656
LCLobe <lloredo.eng@gmail.com>
2023-03-30 20:18:47 +0000

    some relationships + 'names' for ForeingKey

--- src/models.py
+++ src/models.py
@@ -12,36 +12,41 @@ class User(Base):
     __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column("id", Integer, primary_key=True)
-    username = Column(String(250), nullable=False)
+    id = Column("id", Integer(), primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
     firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
+    follower = relationship("Follower", back_populates="user")
+
    
 class Follower(Base):
     __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey(User.id))
-    user_to_id = Column(Integer, ForeignKey(User.id))
+    id = Column(Integer(), primary_key=True)
+    user_from_id = Column(Integer, ForeignKey("user.id"))
+    user_to_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User", back_populates="follower")
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey(User.id))
+    id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), ForeignKey("user.id"))
+    media = relationship("Media", back_populates="post")
 
 class Media(Base):
     __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    type = Column(Enum, nullable=False)
+    id = Column(Integer(), primary_key=True)
+    type = Column(Enum(), nullable=False)
     url = Column(String(250), nullable=False)
-    post_id = Column(Integer, ForeignKey(Post.id))
+    post_id = Column(Integer(), ForeignKey("post.id"))
+    post = relationship("Post", back_populates="media")
 
 class Comment(Base):
     __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer(), primary_key=True)
     comment_text = Column(String(250), nullable=False)
-    author_id = Column(Integer, ForeignKey(User.id))
-    post_id = Column(Integer, ForeignKey(Post.id))
+    author_id = Column(Integer(), ForeignKey("user.id"))
+    post_id = Column(Integer(), ForeignKey("post.id"))
 
     def to_dict(self):
         return {}
